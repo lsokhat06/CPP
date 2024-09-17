ac2cbf37a70b60d49ac5593ecd73e530eba7656f
lsokhat06 <144232187+lsokhat06@users.noreply.github.com>
2024-09-17 17:53:59 +0300

    Update Lab 1.cpp

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -2,24 +2,24 @@
 #include <iostream>
 
 void f(double a, double b) {
-	double x = 1.0;
+    double x = 1.0;
 
-	for (int i = 0; i <= 20; ++i) {
-		double y;
+    for (int i = 0; i <= 20; ++i) {
+        double y;
 
-		if (x < b) {
-			y = sin(abs(a * x + pow(b, a)));
-		}
-		else {
-			y = cos(abs(a * x - pow(b, a)));
-		}
+        if (x < b) {
+            y = sin(abs(a * x + pow(b, a)));
+        }
+        else {
+            y = cos(abs(a * x - pow(b, a)));
+        }
 
-		std::cout << "x = " << x << ", y = " << y << "\n";
+        std::cout << "x = " << x << ", y = " << y << "\n";
 
-		x += 0.1;
-	}
+        x += 0.1;
+    }
 }
 
 int main() {
-	f(1.5, 2.0);
+    f(1.5, 2.0);
 }
